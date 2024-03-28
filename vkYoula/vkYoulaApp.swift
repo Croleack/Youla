a05f278ebc27fe01a6e6348026e2989645d539da
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-03-28 10:23:28 +0500

    Initial Commit

---
+++ vkYoula/vkYoulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vkYoulaApp.swift
+//  vkYoula
+//
+//  Created by Enzhe Gaysina on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct vkYoulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
